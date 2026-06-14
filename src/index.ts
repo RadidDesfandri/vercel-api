9dd8c602365f94c746e9a44bb5f3b074b4674286
RadidDesfandri <radietdesfandri@gmail.com>
2026-06-14 10:22:21 +0700

    feat: add dummy todos

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,26 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
+app.get("/api/v1/todos", (req: Request, res: Response) => {
+  const dummyTodos = [
+    {
+      id: 1,
+      title: "Buy groceries",
+      description: "Buy groceries for the week",
+      completed: false,
+    },
+
+    {
+      id: 2,
+      title: "Buy groceries",
+      description: "Buy groceries for the week",
+      completed: false,
+    },
+  ];
+
+  res.json({ message: "Todos fetched successfully", data: dummyTodos });
+});
+
 if (process.env.NODE_ENV === "development") {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
