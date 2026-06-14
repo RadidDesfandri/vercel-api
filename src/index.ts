8a3216339b58e838d7f8c725f14c1012f1adf680
RadidDesfandri <radietdesfandri@gmail.com>
2026-06-14 10:11:00 +0700

    feat: initial deploy

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+if (process.env.NODE_ENV === "development") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
